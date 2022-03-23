2ed8e5f0b6b0dcda01b3d84f32c05d2633d2b784
Alagić Senad <senad.alagic@edu.fit.ba>
2022-03-23 11:06:28 +0100

    made a few qol functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var input;
 function Search()
 {
+    var wrapper=document.getElementById("wrapper");
+    wrapper.innerHTML="";
     input=document.getElementById("keyword").value;
     var url="https://imdb-api.com/en/API/SearchMovie/k_ecehvy84/"+input;
     console.log("Searched for "+input);
@@ -13,20 +15,7 @@ function Search()
                 r.json().then(
                     t => {
                         t.results.forEach(element => {
-                            var newDiv = document.createElement("div");
-                            newDiv.setAttribute("id", "result")
-
-                            var titleParagaph=document.createElement("p");
-                            var title=document.createTextNode(element.title);
-                            titleParagaph.appendChild(title);
-                            newDiv.appendChild(titleParagaph);
-
-                            var descriptionParagaph=document.createElement("p");
-                            var description=document.createTextNode(element.description);
-                            descriptionParagaph.appendChild(description);
-                            newDiv.appendChild(descriptionParagaph);
-
-                            document.body.appendChild(newDiv);
+                            CreateDiv(element);
                         });
                     });
             }
@@ -34,6 +23,36 @@ function Search()
     .catch(err=>{
         console.log(err);
     });
+}
+function CreateDiv(element)
+{
+    var wrapper=document.getElementById("wrapper");
+    var newDiv=document.createElement("div");
+    newDiv.setAttribute("id","result");
+
+    //add and create a title
+    // var titleParagaph = document.createElement("p");
+    // var title = document.createTextNode(element.title);
+    // titleParagaph.appendChild(title);
+    // newDiv.appendChild(titleParagaph);
+    newDiv.appendChild(CreateParagraph(element.title));
+
+    //add and create a description
+    // var descriptionParagaph = document.createElement("p");
+    // var description = document.createTextNode(element.description);
+    // descriptionParagaph.appendChild(description);
+    // newDiv.appendChild(descriptionParagaph);
+    newDiv.appendChild(CreateParagraph(element.description))
+
+    //append to body
+    wrapper.appendChild(newDiv);
+}
+function CreateParagraph(elementText)
+{
+    var paragraph=document.createElement("p");
+    var text=document.createTextNode(elementText);
+    paragraph.appendChild(text);
+    return paragraph;
 }
 
 window.addEventListener('load', () => {
