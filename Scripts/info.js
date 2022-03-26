21f02637c4d603add0462a25cdc591ffe8d81390
Alagić Senad <senad.alagic@edu.fit.ba>
2022-03-26 17:48:00 +0100

    fixed the file linking

--- Scripts/info.js
+++ Scripts/info.js
@@ -1,4 +1,12 @@
+var movieId;
 function fetchInfo()
 {
-    console.log("fetched!");
+    movieId=sessionStorage.getItem('selectedMovieId');
+    //movie title=document title
+    document.title="";
+    console.log(movieId);
 }
+window.addEventListener('load', () => {
+    console.log("Info is loaded and ready!");
+    fetchInfo();
+});
